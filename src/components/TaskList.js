91deecb3c6a15ade8d8288574e679993d057479f
dickonkudzai <dickonkudzai@live.com>
2023-06-19 13:56:42 +0200

    css changes

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import React,{useState} from "react";
+import styles from './styles.module.css';
 
 function TaskList() {
     const [tasks, setTasks] = useState([]);
@@ -14,24 +15,42 @@ function TaskList() {
     }
   
     return (
-      <div>
+      <div className={styles.container}>
         <h1>Task List</h1>
-        <ul>
-          {tasks.map((task, index) => (
-            <li key={index}>
-              {task}
-              <button onClick={() => deleteTask(index)}>Delete</button>
-            </li>
-          ))}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                ID
+              </th>
+              <th>
+                Title
+              </th>
+              <th>
+                Description
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task, index) => (
+              <tr>
+                <td>{index}</td>
+                <td>{task}</td>
+                <td><button className={styles.btn_danger} onClick={() => deleteTask(index)}>Delete</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <form onSubmit={(event) => {
           event.preventDefault();
           const newTask = event.target.elements.taskInput.value;
           addTask(newTask);
           event.target.reset();
         }}>
-          <input type="text" name="taskInput" />
-          <button type="submit">Add Task</button>
+          <div className={styles.row}>
+            <div><input className={styles.textbox} type="text" name="taskInput" />&nbsp;</div>
+            <div><button className={styles.btn_primary} type="submit">Add Task</button></div>
+          </div>
         </form>
       </div>
     );
